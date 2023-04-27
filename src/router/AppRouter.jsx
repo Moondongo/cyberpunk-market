694944df9a0b528e87f6eb13e8da31c335d54e56
Franco Gabriel Albornoz <albornozfranco800@gmail.com>
2023-04-27 16:03:24 -0300

    prueba error router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -30,7 +30,8 @@ const router = createBrowserRouter([
                 path: '/config',
                 element: <ConfigScreen/>
             }
-        ]
+        ],
+        errorElement: <h1>PAGINA DE ERRROR</h1>
     }
 ])
 
