ae540c0f89cb6014258389e55da4c6bebcee2e01
Franco Gabriel Albornoz <albornozfranco800@gmail.com>
2023-05-03 15:20:46 -0300

    add pause button

--- src/components/ui/ProgressBar.jsx
+++ src/components/ui/ProgressBar.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 
 const ProgressBar = ({progress, isPause, handle}) => {
     return (
-        <div className={`progress-bar_container ${isPause && 'pause'}`} onClick={handle}>
-            <div className='progress-bar_filler' style={{width: `${progress}%`}}>
+        <div className='progress-container'>
+            <div className={`progress-bar_container ${isPause && 'pause'}`}>
+                <div className='progress-bar_filler' style={{width: `${progress}%`}}>
+                </div>
             </div>
+            <button onClick={handle}>
+                {
+                    isPause 
+                        ? <i className="fa-solid fa-pause"></i>
+                        : <i className="fa-solid fa-play"></i>
+                }
+            </button>
         </div>
     )
 }
